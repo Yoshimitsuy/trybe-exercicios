2e0fc20c5a802f9e24378b01c3874358fbf9e2a8
rafaLacana <rafael.lacana7@gmail.com>
2022-02-02 21:04:15 -0300

    exerc para fixar variáveis

---
+++ fundamentos/bloco-04-java-script-e-logica-de-prog/dia-01-java-script-primeiros-passos/scripts.js
@@ -0,0 +1,11 @@
+const myName = "Rafael";
+
+const birthCity = "Rio de janeiro";
+birthCity = "Japão"
+
+let birthYear = 1996;
+birthYear = 2030;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
