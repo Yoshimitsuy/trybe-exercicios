ac5cfe8fa1e065d98de6d3849d772235c40c6574
rafaLacana <rafael.lacana7@gmail.com>
2022-02-02 23:39:46 -0300

    exerc para fixar if e else

--- fundamentos/bloco-04-java-script-e-logica-de-prog/dia-01-java-script-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-java-script-e-logica-de-prog/dia-01-java-script-primeiros-passos/scripts.js
@@ -8,8 +8,7 @@ birthYear = 2030;
 
 console.log(myName);
 console.log(birthCity);
-console.log(birthYear); */
-
+console.log(birthYear); 
 // ---
 
 const base = 5;
@@ -21,3 +20,16 @@ const area = base * height;
 
 const perimeter = 5 + 5 + 8 + 8;
 console.log (perimeter);
+*/
+
+const nota = 99;
+
+if (nota >= 80) {
+    console.log ("Parabéns, você foi aprovado")
+}
+if (nota < 80 && nota >= 60) {
+    console.log ("Você está na nossa lista de espera")
+}
+if (nota < 60) { 
+    console.log ("Você está reprovado")
+}
