cfb670a0b8e359be99cabf91711153379b3ec33f
rafaLacana <rafael.lacana7@gmail.com>
2022-02-11 12:33:35 -0300

    exercícios 5.1

--- fundamentos/bloco-05/dia-01/exercícios/ex5-1.js
+++ fundamentos/bloco-05/dia-01/exercícios/ex5-1.js
@@ -1,1 +1,8 @@
- 
+function contFunction() {
+      
+    let allCont = document.getElementsByTagName('p');
+    for (let index = 0; index < allCont.length; index += 1) {
+        console.log(allCont[index].innerHTML);
+    }
+}
+contFunction();
